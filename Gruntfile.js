fbc9fe6d25df1c9e67a9cf4053eae479cee49b51
unknown <djones6@EIS-TECH-32BNF6.ebsco.com>
2015-03-02 10:03:12 -0600

    Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        ts: {
+            default: {
+                src: 'hoist.ts',
+                outDir: 'dist',
+                options: {
+                    module: 'amd'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.registerTask('default', ['ts']);
+
+};
