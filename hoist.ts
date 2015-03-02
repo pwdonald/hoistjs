b130e0f7e79cf27c84b6eaddd0617f88baf5c825
Donald Jones <jones.donald@gmail.com>
2015-03-01 18:20:45 -0600

    Added comments.

--- hoist.ts
+++ hoist.ts
@@ -1,6 +1,20 @@
-﻿class Hoister {
-    registered = {};
+﻿/**
+    Name: HoistJS
+    Author: Donald Jones <donald@birminghamdeveloper.com>
+    Description: A simple Dependency Injection container.
+ */
+class Hoister {
+    /**
+     * Hash of hoisted class/singletons/statics.
+     */
+    hoisted = {};
 
+    /**
+     * Hoist a class/singleton/static to be retrieved in dependent objects later.
+     * @param {string} name The name to retrieve this object by.
+     * @param {function} construct The object you wish to hoist.
+     * @param {string} type The type of object being hoisted.
+     */
     hoist(name: string, construct: any, type: string = 'SINGLETON') {
         if (!name || name === '') {
             throw new Error('Cannot hoist a constructor with a blank name!');
@@ -13,20 +27,20 @@
         }
 
         if (type === 'SINGLETON') {
-            this.registered[name] = <IHoistable>{
+            this.hoisted[name] = <IHoistable>{
                 name: name,
                 type: 'SINGLETON',
                 construct: construct,
                 instance: new construct()
             };
         } else if (type === 'STATIC') {
-            this.registered[name] = <IHoistable>{
+            this.hoisted[name] = <IHoistable>{
                 name: name,
                 type: 'STATIC',
                 construct: construct
             };
         } else if (type === 'CLASS') {
-            this.registered[name] = <IHoistable>{
+            this.hoisted[name] = <IHoistable>{
                 name: name,
                 type: 'CLASS',
                 construct: construct
@@ -36,18 +50,21 @@
         }
     }
 
+    /*
+     * Pull an object from the hoisted collection.
+     * @param {string} name The name the object was hoisted by.
+     */
     pull(name: string) {
-        var hoisted: IHoistable = this.registered[name];
+        var hoisted: IHoistable = this.hoisted[name];
 
         if (!hoisted) {
             throw new Error(name + ' is not hoisted!');
-            return;
         }
 
         if (hoisted.type === 'STATIC') {
             return hoisted.construct;
         } else if (hoisted.type === 'SINGLETON') {
-            return (hoisted.instance ? hoisted.instance : new hoisted.construct());
+            return hoisted.instance;
         } else if (hoisted.type === 'CLASS') {
             return new hoisted.construct();
         }
